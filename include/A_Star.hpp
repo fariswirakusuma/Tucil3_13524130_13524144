19d56d61e378c78ab4090203c777be157b93ef17
fariswirakusuma <fariswkt@gmail.com>
2026-05-06 07:24:47 +0700

    feat: implement A_Star header

--- include/A_Star.hpp
+++ include/A_Star.hpp
@@ -1,1 +1,35 @@
 #pragma once
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// template <typename T>
+using cord = pair<int,int>;
+
+class Node{
+public:
+    cord c;
+    double g,h,f;
+    Node(cord c, double g, double h):c(c),g(0),h(0){
+        f = g+h;
+    }
+
+};
+struct CompareF {
+    bool operator()(const Node& a, const Node& b) {
+        return a.f > b.f; 
+    }
+};
+
+// template <typename T>
+class A_Star_Solver{
+public:
+
+    cord start;
+    cord end;
+    void expandNode(Node current);
+    priority_queue<Node, vector<Node>, CompareF> openSet();
+};
+
+double calculate_h(cord current, cord end);
+double calculate_g(double parent_g, cord current, cord neighbor);
